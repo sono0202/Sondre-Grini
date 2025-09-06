6740c4e02e1294ca56c71c601a273d71475f733e
Sondre <50902202+sono0202@users.noreply.github.com>
2025-09-06 08:18:40 +0000

    i

--- index.php
+++ index.php
@@ -3,7 +3,7 @@
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1">
-  <title>My Website</title>
+  <title>Sondre Grini</title>
   <!-- Bootstrap CSS -->
   <link 
     href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" 
